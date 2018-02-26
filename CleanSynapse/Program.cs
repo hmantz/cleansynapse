9c71de8d11b78ba9b4742ac5e31b040d04fb3244
GMR516 <garrett@heywordboy.com>
2018-02-26 00:51:42 -0800

    Added copying of program to default installation directory.

--- CleanSynapse/Program.cs
+++ CleanSynapse/Program.cs
@@ -1,4 +1,8 @@
-﻿//using System;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Security.Principal;
 using System.Windows.Forms;
 using Razer.Emily.Common;
 using Razer.Emily.UI;
@@ -7,26 +11,91 @@ using Razer.Storage;
 
 class Program
 {
-    static void Main()
+    static string defaultDirectory = @"C:\Program Files (x86)\Razer\Synapse";
+    static void Main(string[] args)
     {
-        MainStorage mainApp = Singleton<MainStorage>.Instance;
-        mainApp.Initialize(Storage_Project.Emily);
-        if (!mainApp.TryLastLogin())
-            MessageBox.Show("Please login on Razer Synapse first.");
-        else
+        if (File.Exists(AssemblyLocation(true) + "\\RzSynapse.exe"))
         {
-            RzDeviceManager dManager = new RzDeviceManager();
-            dManager.Enumerate();
-            CommonConfigLoader configLoader = Singleton<CommonConfigLoader>.Instance;
-            configLoader.StartConfig();
-            RzDevice[] allDevices = dManager.ActiveDevices.ToArray();
-            foreach (RzDevice rzDevice in allDevices)
+            MainStorage mainApp = Singleton<MainStorage>.Instance;
+            mainApp.Initialize(Storage_Project.Emily);
+            if (!mainApp.TryLastLogin())
+                MessageBox.Show("Please login on Razer Synapse first.");
+            else
             {
-                rzDevice.RefreshData();
-                configLoader.DeviceAdded(rzDevice.VID, rzDevice.PID);
-                //PluginDevice pluginDevice = configLoader.FindDevice(rzDevice.PID);
-                //Console.WriteLine("Loaded: " + pluginDevice.Name);
+                RzDeviceManager dManager = new RzDeviceManager();
+                dManager.Enumerate();
+                CommonConfigLoader configLoader = Singleton<CommonConfigLoader>.Instance;
+                configLoader.StartConfig();
+                RzDevice[] allDevices = dManager.ActiveDevices.ToArray();
+                foreach (RzDevice rzDevice in allDevices)
+                {
+                    rzDevice.RefreshData();
+                    configLoader.DeviceAdded(rzDevice.VID, rzDevice.PID);
+                    //PluginDevice pluginDevice = configLoader.FindDevice(rzDevice.PID);
+                    //Console.WriteLine("Loaded: " + pluginDevice.Name);
+                }
             }
         }
+        else
+        {
+            DialogResult result = DialogResult.None;
+            if (args.Length < 1)
+            {
+                if (Directory.Exists(defaultDirectory))
+                    result = MessageBox.Show("The program is not in the Synapse folder. Automatically move it?", "Synapse Was Not Found", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+                else
+                {
+                    MessageBox.Show("The program is not in the Synapse folder, and we can't find the directory. You probably installed it somewhere else. Please manually move the program to the root of the Razer Synapse directory.");
+                    return;
+                }
+            }
+            if (result == DialogResult.Yes || args.Length > 0)
+            {
+                WindowsPrincipal pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+                bool hasAdministrativeRight = pricipal.IsInRole(WindowsBuiltInRole.Administrator);
+                if (!hasAdministrativeRight)
+                {
+                    string fileName = Assembly.GetExecutingAssembly().Location;
+                    ProcessStartInfo processInfo = new ProcessStartInfo
+                    {
+                        Verb = "runas",
+                        Arguments = "-true",
+                        FileName = fileName
+                    };
+                    try
+                    {
+                        Process.Start(processInfo);
+                    }
+                    catch
+                    {
+                        //Thrown if cancelled.
+                        MessageBox.Show("The program needs admin to move the file to your program files.");
+                        MessageBox.Show("The program will now exit.");
+                        return;
+                    }
+                    return;
+                }
+                else
+                {
+                    string path = defaultDirectory + "\\" + Path.GetFileName(AssemblyLocation(false));
+                    File.Copy(AssemblyLocation(false), path, true);
+                    Process.Start(path);
+                    return;
+                }
+            }
+            if (result == DialogResult.No)
+            {
+                MessageBox.Show("The program will now exit.");
+                return;
+            }
+        }
+    }
+
+    public static string AssemblyLocation(bool directory)
+    {
+        string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+        UriBuilder uri = new UriBuilder(codeBase);
+        string path = Uri.UnescapeDataString(uri.Path);
+        return directory ? Path.GetDirectoryName(path) : path;
     }
 }
--- CleanSynapse/Properties/AssemblyInfo.cs
+++ CleanSynapse/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("CleanSynapse")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("GMR")]
+[assembly: AssemblyProduct("CleanSynapse")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
