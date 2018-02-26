d50e840fa83bc7255d257eb2b361c3849b618f6e
GMR516 <garrett@heywordboy.com>
2018-02-26 00:00:36 -0800

    Version 1.

--- CleanSynapse/Program.cs
+++ CleanSynapse/Program.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+using System.Windows.Forms;
+using Razer.Emily.Common;
+using Razer.Emily.UI;
+//using Razer.Emily.UISDK;
+using Razer.Storage;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-
+        MainStorage mainApp = Singleton<MainStorage>.Instance;
+        mainApp.Initialize(Storage_Project.Emily);
+        if (!mainApp.TryLastLogin())
+            MessageBox.Show("Please login on Razer Synapse first.");
+        else
+        {
+            RzDeviceManager dManager = new RzDeviceManager();
+            dManager.Enumerate();
+            CommonConfigLoader configLoader = Singleton<CommonConfigLoader>.Instance;
+            configLoader.StartConfig();
+            RzDevice[] allDevices = dManager.ActiveDevices.ToArray();
+            foreach (RzDevice rzDevice in allDevices)
+            {
+                rzDevice.RefreshData();
+                configLoader.DeviceAdded(rzDevice.VID, rzDevice.PID);
+                //PluginDevice pluginDevice = configLoader.FindDevice(rzDevice.PID);
+                //Console.WriteLine("Loaded: " + pluginDevice.Name);
+            }
+        }
     }
 }
